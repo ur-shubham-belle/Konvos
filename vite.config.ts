11db2fc23bafdacbb3dd25823332dc1678d05be0
ur-shubham-belle <belleshubham106@gmail.com>
2025-11-23 19:43:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { youware } from '@youware/vite-plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    youware()
+    react()
   ],
   resolve: {
     alias: {
